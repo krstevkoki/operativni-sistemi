e20feff248c39f55d6106f06ef6e0de4b61c8725
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-03-09 18:52:12 +0100

    changed the regex, removed the unnecessary normalizeWord() method

--- src/mk/ukim/finki/lab1/HW01_4.java
+++ src/mk/ukim/finki/lab1/HW01_4.java
@@ -13,10 +13,9 @@ public class HW01_4 {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
                 String line;
                 while ((line = reader.readLine()) != null && line.length() > 0) {
-                    String[] lineParts = line.split("\\s+");
+                    String[] lineParts = line.split("\\W+");
                     for (String linePart : lineParts) {
-                        String normalizedWord = normalizeWord(linePart.trim());
-                        if (normalizedWord.equals(word))
+                        if (linePart.equals(word))
                             ++counter;
                     }
                 }
@@ -26,19 +25,6 @@ public class HW01_4 {
             System.err.println("The given file is a directory or does not exists.");
             return -1;
         }
-    }
-
-    /**
-     * Removes the last character, for a word, if is not a letter or digit.
-     *
-     * @param word A word to be normalized
-     */
-    private static String normalizeWord(String word) {
-//        if (!Character.isLetter(word.charAt(word.length() - 1)))
-        if (!Character.isLetterOrDigit(word.charAt(word.length() - 1)))
-            return word.substring(0, word.length() - 1);
-        else
-            return word;
     }
 
     public static void main(String[] args) throws IOException {
